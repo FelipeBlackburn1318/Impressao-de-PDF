26dc40e12c98d01409fe0829c6689c2a7caafa0b
Felipe Henrique Astolfi Bazon <felipeastolfi@gmail.com>
2016-12-06 11:31:28 -0200

    Versão 1.0.1 Concluída

---
+++ Impressao PDF/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Impressao_PDF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnAdiciona = new System.Windows.Forms.Button();
+            this.btnImprime = new System.Windows.Forms.Button();
+            this.dtgPdf = new System.Windows.Forms.DataGridView();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgPdf)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAdiciona
+            // 
+            resources.ApplyResources(this.btnAdiciona, "btnAdiciona");
+            this.btnAdiciona.BackColor = System.Drawing.Color.DarkRed;
+            this.btnAdiciona.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btnAdiciona.Name = "btnAdiciona";
+            this.btnAdiciona.UseVisualStyleBackColor = false;
+            this.btnAdiciona.Click += new System.EventHandler(this.btnAdiciona_Click);
+            // 
+            // btnImprime
+            // 
+            resources.ApplyResources(this.btnImprime, "btnImprime");
+            this.btnImprime.BackColor = System.Drawing.Color.DarkGreen;
+            this.btnImprime.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btnImprime.Name = "btnImprime";
+            this.btnImprime.UseVisualStyleBackColor = false;
+            this.btnImprime.Click += new System.EventHandler(this.btnImprime_Click);
+            // 
+            // dtgPdf
+            // 
+            resources.ApplyResources(this.dtgPdf, "dtgPdf");
+            this.dtgPdf.AllowUserToAddRows = false;
+            this.dtgPdf.AllowUserToDeleteRows = false;
+            this.dtgPdf.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dtgPdf.BackgroundColor = System.Drawing.Color.DarkSlateGray;
+            this.dtgPdf.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgPdf.Name = "dtgPdf";
+            // 
+            // btnLimpar
+            // 
+            resources.ApplyResources(this.btnLimpar, "btnLimpar");
+            this.btnLimpar.BackColor = System.Drawing.Color.DarkRed;
+            this.btnLimpar.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.UseVisualStyleBackColor = false;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label1.Name = "label1";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.dtgPdf);
+            this.Controls.Add(this.btnImprime);
+            this.Controls.Add(this.btnAdiciona);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dtgPdf)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdiciona;
+        private System.Windows.Forms.Button btnImprime;
+        private System.Windows.Forms.DataGridView dtgPdf;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Impressao PDF/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Impressao_PDF
+{
+    public partial class Form1 : Form
+    {
+        String[] arquivo = new String[999];
+        int contador = 0;
+        int contadorTabela = 0;
+        int maximo = 999;
+        DataTable tabela = new DataTable();
+        String[] erro = new String[999];
+
+        public Form1()
+        {
+            InitializeComponent();
+            try
+            {
+                tabela.Columns.Add("Ordem de impressão", typeof(int));
+                tabela.Columns.Add("Nome do arquivo", typeof(String));
+                carregarLista();
+
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void btnAdiciona_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (contador < maximo)
+                {
+                    OpenFileDialog arqu = new OpenFileDialog();
+
+                    arqu.Filter = "arquivos PDF |*.pdf";
+                    arqu.Multiselect = true;
+
+                    if (arqu.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                    {
+                        foreach (String file in arqu.FileNames)
+                        {
+                            arquivo[contador] = System.IO.Path.GetFullPath(file);
+                            carregarLista(System.IO.Path.GetFileName(file));
+                            contador++;
+                        }
+                    }
+
+                }
+            }
+            catch
+            {
+            }
+        }
+
+        public void carregarLista()
+        {
+            try
+            {
+                dtgPdf.DataSource = tabela;
+            }
+            catch
+            {
+
+            }
+        }
+
+
+        public void carregarLista(String nome)
+        {
+            try
+            {
+                contadorTabela++;
+                tabela.Rows.Add(contadorTabela, nome);
+
+                dtgPdf.DataSource = tabela;
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void btnImprime_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Process p = new Process();
+                int total = contador;
+                contador = 0;
+                while (contador < total)
+                {
+                    p.StartInfo.Verb = "print";
+                    p.StartInfo.CreateNoWindow = true;
+                    p.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                    p.StartInfo.FileName = arquivo[contador];
+                    p.Start();
+                    p.WaitForInputIdle();
+                    try
+                    {
+                        System.Threading.Thread.Sleep(7000);
+                        if (false == p.CloseMainWindow())
+                            p.Kill();
+                    }
+                    catch
+                    {
+                    }
+
+                    contador++;
+                }
+
+                limparLista();
+            }
+            catch
+            {
+            }
+        }
+
+        public void limparLista()
+        {
+            try
+            {
+                tabela = new DataTable();
+                tabela.Columns.Add("Ordem de impressão", typeof(int));
+                tabela.Columns.Add("Nome do arquivo", typeof(String));
+                dtgPdf.DataSource = tabela;
+                contadorTabela = 0;
+                contador = 0;
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            limparLista();
+        }
+    }
+}
